eb0a8c93613008609d70f0ce0d9e1fd40741b15a
david042 <david.mal04@hotmail.com>
2021-06-13 04:05:50 +0900

    agora ta funcional

--- www/components/acao.js
+++ www/components/acao.js
@@ -1,2 +1,18 @@
-// This is a JavaScript file
+$(document).on("click","#buscar",function(){
+  $.ajax({
+    type:"get",
+    url:"https://viacep.com.br/ws/"+$("#cep").val()+"/json/",
+    success: function(data){
+      var conteudo;
+      conteudo = "<p><strong>Logradouro: </strong>"+data.logradouro+"</p>"
+      conteudo += "<p><strong>Bairro: </strong>"+data.bairro+"</p>"
+      conteudo += "<p><strong>Cidade: </strong>"+data.localidade+"</p>"
+      conteudo += "<p><strong>Estado: </strong>"+data.uf+"</p>"
 
+      $("#dados").html(conteudo);
+    },
+    error: function(){
+      alert("deu erro!");
+    }
+  });
+});
